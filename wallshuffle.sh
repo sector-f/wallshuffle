42e1b1131aeb36f5de1bd1bb2aa0ce26288b874a
Adam <adamsimeth@gmail.com>
2016-01-03 01:54:45 -0500

    Added single-image mode

--- wallshuffle.sh
+++ wallshuffle.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # Okay, let's start with some variables
-
 # If the first argument is a directory, then we'll use that
+# If it's a file, we'll just set the wallpaper to that file
 # Otherwise, exit
 if [[ -d "$1" ]]; then
 	dir=$1
+elif [[ -f "$1" ]]; then
+	file=$1
 else
-	echo "No directory specified"
+	echo "No directory or file specified"
 	exit 1
 fi
 
@@ -22,7 +24,7 @@ fi
 
 # First we get every image in the directory and shuffle them
 # We'll make this a function so we can call it more easily
-shuffle() {
+getimages() {
 	for image in $dir/*.{jpg,jpeg,png}; do
 		[[ -f "$image" ]] && echo "$image"
 	done | shuf
@@ -30,9 +32,17 @@ shuffle() {
 
 # Then we actually start setting them as a background
 # If we set an interval, we'll use that instead of 60
-while true; do
-	for image in $(shuffle); do
-		feh --bg-fill "$image"
-		sleep ${interval:-60}
+shuffle() {
+	while true; do
+		for image in $(getimages); do
+			feh --bg-fill "$image"
+			sleep ${interval:-60}
+		done
 	done
-done
+}
+
+if [[ -n $file ]]; then
+	feh --bg-fill $file
+else
+	shuffle
+fi
