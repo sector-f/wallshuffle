ab69f0626ae4525c9b4de0ddc5b86525a54b9d8d
Adam <adamsimeth@gmail.com>
2016-01-03 03:44:36 -0500

    Added support for multiple dirs/files at once

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-wallshuffle.sh
+### wallshuffle.sh
 
-Give directory as first argument
+Give one or more directories or images as arguments
 
-Optionally give wait time in seconds as second argument (default is 60)
+Use `-i <NUMBER>` or `--interval <NUMBER>` to specify the wait time between changing wallpapers, in seconds. The default is 60.
 
 Currently only supports jpg and png, until I can find a complete list of filetypes supported by feh
 
-TODO:
-Add support for multiple directories?
-Figure out which filetypes feh/Imlib2 actually supports
-Format README I guess
+####TODO
+- Add support for image names with spaces
+- Figure out which filetypes feh/Imlib2 actually supports
--- wallshuffle.sh
+++ wallshuffle.sh
@@ -1,48 +1,42 @@
 #!/usr/bin/env bash
 
-# Okay, let's start with some variables
-# If the first argument is a directory, then we'll use that
-# If it's a file, we'll just set the wallpaper to that file
-# Otherwise, exit
-if [[ -d "$1" ]]; then
-	dir=$1
-elif [[ -f "$1" ]]; then
-	file=$1
-else
-	echo "No directory or file specified"
-	exit 1
-fi
+SHORT="i:"
+LONG="interval:"
+OPTIONS=$(getopt -o "$SHORT" -l "$LONG" -n "$(basename $0)" -- "$@")
+eval set -- "$OPTIONS"
 
-# Now we'll see if the second arg is an integer
-# If it is, we'll use it as the time between image changes (in seconds)
-# If it isn't...eh, who cares? We can just default to 60
-if [[ "$2" =~ ^-?[0-9]+$ ]]; then
-	interval="$2"
-fi
+shopt -s nullglob
 
-# Now for the fun part!
+for argument in $@; do
+	case "$1" in
+		-i|--interval)
+			interval="$2"
+			shift 2
+			;;
+		*)
+			if [[ -d "$1" ]]; then
+				images+=("$(dirname $1)/$(basename $1)"/*.{jpg,jpeg,png})
+			elif [[ -f "$1" ]]; then
+				images+=("$1")
+			fi
+			shift
+			;;
+	esac
+done
 
-# First we get every image in the directory and shuffle them
-# We'll make this a function so we can call it more easily
-getimages() {
-	for image in $dir/*.{jpg,jpeg,png}; do
-		[[ -f "$image" ]] && echo "$image"
+shuffleimages() {
+	for image in "${images[@]}"; do
+		echo $image
 	done | shuf
 }
 
-# Then we actually start setting them as a background
-# If we set an interval, we'll use that instead of 60
-shuffle() {
+setbackground() {
 	while true; do
-		for image in $(getimages); do
+		for image in $(shuffleimages); do
 			feh --bg-fill "$image"
 			sleep ${interval:-60}
 		done
 	done
 }
 
-if [[ -n $file ]]; then
-	feh --bg-fill $file
-else
-	shuffle
-fi
+setbackground
